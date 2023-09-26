dbd44968bec04143e8778509dddf4e82ba4c3774
seyed0123 <seyed123ali123@gmail.com>
2023-09-26 13:38:24 +0330

    adding ai api

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module OXDebate
 
 go 1.19
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/sashabaranov/go-openai v1.15.3
+)
--- src/main.go
+++ src/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	openai "github.com/sashabaranov/go-openai"
 )
+
+var key string
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -42,6 +48,35 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	apiKey, err := ioutil.ReadFile("config.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	key = string(apiKey)
 	http.HandleFunc("/ws", wsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+func APICall(msg string) string {
+	client := openai.NewClient(key)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: msg,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return ""
+	}
+	fmt.Println("answer for this text %v : %v \n", msg, resp.Choices[0].Message.Content)
+	return resp.Choices[0].Message.Content
+
 }
